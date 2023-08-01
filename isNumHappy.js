6180e556ce393a3059a7ce79b193823dd850757f
Kishan G <128810424+kaizen-01@users.noreply.github.com>
2023-08-01 17:30:31 +0530

    Add files via upload

---
+++ isNumHappy.js
@@ -0,0 +1,23 @@
+function numInAray(num) {
+    let numArr = [];
+    let numStr = num.toString();
+    for (let i of numStr) {
+      numArr.push(parseInt(i));
+    }
+    return numArr;
+  }
+  
+  function isNumHappy(Num) {
+    let arr = numInAray(Num).map(ele => ele ** 2);
+    let ans = arr.reduce((i,j) =>{return i+j})
+    if (ans >= 10) {
+      return isNumHappy(ans);
+    }
+    else if (ans < 10 && ans == 1) {
+      return true;
+    }
+    else {
+      return false;
+    }
+  } 
+
